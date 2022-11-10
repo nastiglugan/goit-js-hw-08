ab0d7b2c7766c97ca76d8489a557c4f7c80f76c8
NastiHluhan <0506167197more@gmail.com>
2022-11-10 23:08:02 +0200

    add js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,58 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => ` <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+let instance;
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.description}"/> `,
+    {
+      onShow: instance => {
+        document.addEventListener('keydown', onModalClose);
+      },
+      onClose: instance => {
+        document.removeEventListener('keydown', onModalClose);
+      },
+    }
+  );
+  instance.show();
+
+  {
+  }
+}
+
+function onModalClose(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+  }
+}
+gallery.addEventListener('click', onClick);
