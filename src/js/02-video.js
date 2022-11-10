48c28053cfa2036a2109f1986bfe99b18f8b1906
NastiHluhan <0506167197more@gmail.com>
2022-11-11 01:21:51 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function onPlay(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
