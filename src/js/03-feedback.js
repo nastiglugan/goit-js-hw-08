36777430f8f75c1dc07cb2aa07e9e0a161dd1f77
NastiHluhan <0506167197more@gmail.com>
2022-11-11 22:25:46 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(getFormValue, 500));
+form.addEventListener('submit', resetForm);
+window.addEventListener('load', onLoadPage);
+
+//1
+function getFormValue(evt) {
+  const formValue = {
+    email: evt.currentTarget.email.value,
+    message: evt.currentTarget.message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValue));
+}
+
+//2
+function onLoadPage(evt) {
+  if (localStorage.getItem('feedback-form-state')) {
+    const { email, message } = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    form.email.value = email;
+    form.message.value = message;
+  }
+}
+
+//3
+function resetForm(evt) {
+  evt.preventDefault();
+
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+
+  evt.currentTarget.reset();
+  localStorage.clear();
+}
